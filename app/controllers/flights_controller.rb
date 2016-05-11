71add19294242a65fdcb1708b74727535971f38d
destroyergm <stefan_vg@hotmail.com>
2016-05-11 21:08:11 +0200

    Add form hash indexing

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,12 @@
 class FlightsController < ApplicationController
+	include FlightsHelper
+
 	def index
+		if params_exist?
+			@query = Airport.first
+		end
 		@airports = Airport.all
 		@flights = Flight.all
 	end
+
 end
--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,2 +1,9 @@
 module FlightsHelper
+		def params_exist?
+			if !params[:search].blank?
+				return true
+			else
+				return false
+			end
+	end
 end
