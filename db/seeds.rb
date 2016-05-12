538c394b1d0b305804fcb838bf4a778f01035859
destroyergm <stefan_vg@hotmail.com>
2016-05-12 19:44:27 +0200

    Add Radio form-table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,22 @@ Airport.create(code: "PAR")
 
 # Create test flights
 Flight.create(from_airport_id: 1, to_airport_id: 2, 
-	datetime: "18:00 12/5/2016", duration: 3)
+	datetime: "18:15 12/5/2016", duration: 2)
+Flight.create(from_airport_id: 1, to_airport_id: 2, 
+	datetime: "18:30 12/5/2016", duration: 2)
+Flight.create(from_airport_id: 1, to_airport_id: 2, 
+	datetime: "18:45 12/5/2016", duration: 2)
+Flight.create(from_airport_id: 1, to_airport_id: 2, 
+	datetime: "19:00 12/5/2016", duration: 2)
+Flight.create(from_airport_id: 1, to_airport_id: 2, 
+	datetime: "20:00 12/5/2016", duration: 2)
+Flight.create(from_airport_id: 1, to_airport_id: 3, 
+	datetime: "20:30 12/5/2016", duration: 1)
+Flight.create(from_airport_id: 1, to_airport_id: 4, 
+	datetime: "21:30 12/5/2016", duration: 3)
+Flight.create(from_airport_id: 1, to_airport_id: 5, 
+	datetime: "05:00 12/5/2016", duration: 3)
+
 Flight.create(from_airport_id: 2, to_airport_id: 3, 
 	datetime: "18:00 14/3/2016", duration: 1)
 Flight.create(from_airport_id: 2, to_airport_id: 4, 
